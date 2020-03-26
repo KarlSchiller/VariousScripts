7b58182c3ed616edeedd4f82ddc6aef7898437a5
Karl Schiller <karl9000@gmx.de>
2020-03-26 15:52:15 +0100

    Recognises files with same filename
    Files with same filename should have also the same name after the
    renaming process. This is now implemented. The option --individual or -i
    enables the old behavior skip this namecheck.

--- rename_files.py
+++ rename_files.py
@@ -3,6 +3,8 @@
 # Although filenames may start at different index upon successive iterations of the script,
 # the files themselves remain unchanged.
 
+# Requires Python 3.7 -> dicts have to be ordered
+
 import os
 import time
 import argparse
@@ -22,30 +24,46 @@ def get_filelist(input_folder):
     return filelist
 
 
+def get_filename(fullname):
+    '''Split the file extension from the filename'''
+    if "." in fullname:
+        return fullname.split(".")[0]
+    else:
+        return fullname
+
+
 def rename_files(filelist, args):
     '''Rename all files in filelist as specified in args'''
+    # list of filenames without extension to check for dublicates
+    filenames = list(map(get_filename, filelist))
+    current_index = 0
+
     for index,item in enumerate(filelist):
-        last_index = index
+
         extension = ""
         if "." in item:
             extension = "." + item.split(".")[-1]
 
         old_file = os.path.join(args.input_folder,item)
-        new_file = os.path.join(args.input_folder,args.prefix + "{:0>4d}".format(index) + extension)
+        if not args.individual:
+            if filenames[index-1]==filenames[index]:
+                current_index -= 1
+        new_file = os.path.join(args.input_folder,args.prefix + "{:0>4d}".format(current_index) + extension)
 
         while os.path.isfile(new_file): # is there already a file named new_file?
-              last_index += 1
-              new_file = os.path.join(args.input_folder,args.prefix + "{:0>4d}".format(last_index) + extension)
+              current_index += 1
+              new_file = os.path.join(args.input_folder,args.prefix + "{:0>4d}".format(current_index) + extension)
 
         os.rename(old_file,new_file)
         if args.verbose:
             print(old_file + " renamed to " + new_file)
+        current_index += 1
 
 
 def main(args):
     files = get_filelist(args.input_folder)
 
-    # prevent dublicate filenames by adding timestamp to old ones
+    #  prevent dublicate filenames by adding timestamp to old ones
     timestamp = str(int(time.time()))
     for item in files:
         os.rename(os.path.join(args.input_folder,item),
@@ -58,16 +76,19 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A script to rename multiple files.")
+    parser = argparse.ArgumentParser(description="A script to rename multiple files. \
+            Files with the same name will be renamed same as well with default options.")
     parser.add_argument("input_folder", type=str,
             help="Folder containing files to rename")
     group = parser.add_mutually_exclusive_group()   # either verbose or quiet
     group.add_argument("-v", "--verbose", action="store_true",
             help="increase output verbosity")
-    group.add_argument("-q", "--quiet", action="store_true",
-            help="decrease output verbosity")
+    #  group.add_argument("-q", "--quiet", action="store_true",
+            #  help="decrease output verbosity")
     parser.add_argument("-p", "--prefix", type=str, default="",
             help="String in each filename before ongoing number")
+    parser.add_argument("-i", "--individual", action="store_true",
+            help="Rename each file individually")
 
     args = parser.parse_args()
 
