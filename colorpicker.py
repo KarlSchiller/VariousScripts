44858c88697705f2c04b21b1b87d5ec6d1a18035
Karl Schiller <karl9000@gmx.de>
2020-04-28 23:42:56 +0200

    Add argument parsing
    argument is the path to the desired image
    optional arguments are the help function and increase of output
    verbosity
    
    OpenCV is imported as a reminder to use it for this script

--- colorpicker.py
+++ colorpicker.py
@@ -1,3 +1,25 @@
 # General description:
 # Open an image and receive a pixels color by clicking on it
 # The image is given as an keyword argument
+
+import cv2
+import argparse
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+            description="A script to pick a color from a picture.")
+    parser.add_argument("imagepath", type=str,
+            help="path to image to pick a color from")
+    parser.add_argument("-v", "--verbose", action="store_true",
+            help="increase output verbosity")
+
+    args = parser.parse_args()
+
+    main(args)
+    pass
