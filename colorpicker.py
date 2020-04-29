2ff6d2a7e01c67adb337434a944749b167b9ba7f
Karl Schiller <karl9000@gmx.de>
2020-04-29 17:26:51 +0200

    Import image and resize it to neat dimension
    tkinter is used to read the monitor dimensions.
    The picture is shown with either 90% of the screen height or 90% of the
    screen height, depending on the picture's shape.

--- colorpicker.py
+++ colorpicker.py
@@ -2,12 +2,42 @@
 # Open an image and receive a pixels color by clicking on it
 # The image is given as an keyword argument
 
-import cv2
-import argparse
-
+import argparse     # read input image path
+import cv2  # image manipulation
+from tkinter import Tk  # get monitor size
 
 def main(args):
-    pass
+
+    img = cv2.imread(args.imagepath, 1)   # import image colored withouth transparency
+    # (:,:,0) blue channel
+    # (:,:,1) green channel
+    # (:,:,2) red channel
+    print('Image dimensions : ', img.shape)
+    img_scale = img.shape[0]/img.shape[1]   # width/height
+
+    # get monitor size
+    root = Tk()
+    monitor_width = root.winfo_screenwidth()
+    monitor_height = root.winfo_screenheight()
+    print('monitor width  ', monitor_width)
+    print('monitor height ', monitor_height)
+
+    # rescale image to adequate size
+    # 90% of monitor width or height, depending which side is longer
+    height = int(0.9*monitor_height)
+    width = int(height/img_scale)
+    if width > (0.95*monitor_width):
+        print("TEST")
+        width = int(0.9*monitor_width)
+        height = int(width*img_scale)
+    img = cv2.resize(img, (width, height))
+
+    # just show image
+    cv2.imshow('Test', img)
+    print("\nPress any key to quit\n")
+    key = cv2.waitKey(0) & 0xFF
+    cv2.destroyWindow('Test')
+    print("pressed key :  ", key)
 
 
 if __name__ == '__main__':
